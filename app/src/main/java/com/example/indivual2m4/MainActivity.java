3beca755b5df9440f1dd98ec9d9adf42a86d197c
Roberto Medina P <dakiu8@hotmail.com>
2023-06-02 13:38:47 -0400

    initial commit

---
+++ app/src/main/java/com/example/indivual2m4/MainActivity.java
@@ -0,0 +1,15 @@
+package com.example.indivual2m4;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+
+    }
+}
